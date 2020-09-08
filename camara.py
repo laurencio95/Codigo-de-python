7329769dde66acdf9deb9ec9f6cfafa9c87632eb
laurencio95 <44822601+laurencio95@users.noreply.github.com>
2020-09-08 14:05:11 -0500

    Add files via upload

---
+++ camara.py
@@ -0,0 +1,54 @@
+import PySimpleGUI as sg
+import numpy as np
+from datetime import date
+import urllib.request
+import cv2
+
+#Se declara la url de manera indirecta.
+#url=('http://192.168.137.203/cam-lo.png')
+
+def main():
+    #Conectamos a la camara con la url directa
+    camara = cv2.VideoCapture('http://192.168.137.48/cam-lo.png')
+    #camara = cv2.VideoCapture(0)
+
+    #Elegimos un tema de PySimpleGUI
+    sg.theme('DarkGreen5')
+    #Definimos los elementos de la interfaz grafica
+    layout = [[sg.Image(filename='', key='-image-')],
+              [sg.Button('Tomar Fotografia'),sg.Button('Salir')]]
+    #Creamos la interfaz grafica
+    window = sg.Window('Camara ESP32-OV2640',
+                       layout,
+                       no_titlebar=False,
+                       location=(0, 0))
+
+    image_elem = window['-image-']
+
+    numero = 0
+    #Iniciamos la lectura y actualizacion
+    while camara.isOpened():
+        #Obtenemos informacion de la interfaz grafica y video
+        event, values = window.read(timeout=0)
+        ret, frame = camara.read()
+        #tecla = cv2.waitKey(5) & 0xFF
+       
+        #Si salimos
+        #if tecla ==27:
+         #   break
+        if event in ('Exit', None):
+            break
+
+        #Si tomamos foto
+        elif event == 'Tomar Fotografia':
+            ruta = sg.popup_get_folder(title='Guardar Fotografia', message="Carpeta destino")
+            cv2.imwrite(ruta + "/" + str(date.today()) + str(numero) + ".png", frame)
+
+        if not ret:
+            break
+
+        #Mandamos el video a la GUI
+        imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
+        image_elem.update(data=imgbytes)
+        numero = numero + 1
+main()
---
+++ esp32Code.ino
@@ -0,0 +1,89 @@
+
+#include <esp32cam.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
+const char* WIFI_SSID = "lauro";
+const char* WIFI_PASS = "lauro1995";
+
+WebServer server(80);
+
+static auto loRes = esp32cam::Resolution::find(800, 600);
+static auto hiRes = esp32cam::Resolution::find(1024, 768);
+
+void serveJpg()
+{
+   auto frame = esp32cam::capture();
+   if (frame == nullptr){
+     Serial.println("CAPTURE FAIL");
+     server.send(503, "", "");
+     return;
+   }
+   Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                  static_cast<int>(frame->size()));
+
+   server.setContentLength(frame->size());
+   server.send(200, "image/png");
+   WiFiClient client = server.client();
+   frame->writeTo(client);
+}
+
+void handleJpgLo()
+{
+  if (!esp32cam::Camera.changeResolution(loRes)){ 
+    Serial.println("SET-LO-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)){ 
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+
+ WiFi.persistent(false);
+ WiFi.mode(WIFI_STA);
+ WiFi.begin(WIFI_SSID, WIFI_PASS);
+ while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+
+ }
+
+ Serial.print("http://");
+ Serial.print(WiFi.localIP());
+ Serial.println("/cam-lo.png");
+
+ Serial.print("http://");
+ Serial.print(WiFi.localIP());
+ Serial.println("/cam-hi.png");
+
+ server.on("/cam-lo.png", handleJpgLo);
+ server.on("/cam-hi.png", handleJpgHi);
+
+ server.begin();
+ }
+ void loop()
+ {
+  server.handleClient();
+}
+
